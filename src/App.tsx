a6a944bdf2bbb5071a48510bb33453345ddee5b6
Burak Sakalli <buraksakalli06@gmail.com>
2022-08-16 00:19:37 +0400

    FIX: Scene dimension fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import Spline from "@splinetool/react-spline";
 
 export default function App() {
   return (
-    <div
+    <section
       style={{
-        height: "100vh",
+        flex: 1,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <Spline scene="https://prod.spline.design/jx-FkcOIT3d0JgB6/scene.splinecode" />
-    </div>
+      <section>
+        <h1>iPhone 13 Pro Max 256GB</h1>
+        <h2>2599 AED</h2>
+      </section>
+      <section>
+        <Spline scene="https://prod.spline.design/jx-FkcOIT3d0JgB6/scene.splinecode" />
+      </section>
+    </section>
   );
 }
